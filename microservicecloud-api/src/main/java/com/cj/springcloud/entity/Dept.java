72fc7128c872927721d220a3d9dd5ecd773a9b2a
chenajun1234 <499187079@qq.com>
2019-03-07 00:19:48 +0800

    create spring cloud demo

---
+++ microservicecloud-api/src/main/java/com/cj/springcloud/entity/Dept.java
@@ -0,0 +1,17 @@
+package com.cj.springcloud.entity;
+
+import java.io.Serializable;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.Accessors;
+
+@Accessors(chain=true)
+@Data
+@NoArgsConstructor
+public class Dept implements Serializable {
+	private Long deptno; // 主键
+	private String dname; // 部门名称
+	private String db_source;// 来自那个数据库，因为微服务架构可以一个服务对应一个数据库，同一个信息被存储到不同数据库
+
+}
